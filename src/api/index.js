c58f85ed57ff379dc14cde1ae4267cfc020d47db
chengh <chengh@ebs.org.cn>
2019-09-09 18:07:12 +0800

    day4

--- src/api/index.js
+++ src/api/index.js
@@ -3,3 +3,9 @@ const BASE = 'api'
 // 获取一级分类或者二级分类
 export const reqCategory = (params) => request(BASE + '/manage/category/list', params)
 
+// 添加一级分类或二级分类
+export const reqAddCategory = (params) => request(BASE + '/manage/category/add', params, 'post')
+
+// 更新一级分类或二级分类名称
+export const reqUpdateCategoryName = (params) => request(BASE + '/manage/category/update', params, 'post')
+
--- src/views/category/index.vue
+++ src/views/category/index.vue
@@ -8,13 +8,13 @@
           <i class="el-icon-right" style="margin-left: 10px; font-size: 18px"></i>
           <span>{{categoryName}}</span>
         </span>
-        <el-button @click="dialogFormVisible=true" style="float: right; padding: 8px 12px; margin-top: -4px;" icon="el-icon-plus" type="primary">添加品类</el-button>
+        <el-button @click="showAddCategory=true" style="float: right; padding: 8px 12px; margin-top: -4px;" icon="el-icon-plus" type="primary">添加品类</el-button>
       </div>
 
-      <!-- 一级分类展示 -->
+      <!-- 一级分类表格显示 -->
       <template v-if="!isShowSubCategory">
         <el-table
-          :data="categoryData"
+          :data="categoryPageData"
           border
           >
           <el-table-column
@@ -27,7 +27,7 @@
             <template slot-scope="scope">
               <el-button
                 type="text"
-                @click="updateCategoryName(scope)">修改名称</el-button>
+                @click="updateCategoryName(scope, 0)">修改名称</el-button>
               <el-button
                 type="text"
                 @click="showSubCategory(scope.row)">查看其子品类</el-button>
@@ -37,7 +37,7 @@
         </el-table>
       </template>
 
-      <!-- 二级分类展示 -->
+      <!-- 二级分类表格显示 -->
       <template v-if="isShowSubCategory">
         <el-table
           :data="subCategoryData"
@@ -53,7 +53,7 @@
             <template slot-scope="scope">
               <el-button
                 type="text"
-                @click="updateCategoryName(scope)">修改名称
+                @click="updateCategoryName(scope, 1)">修改名称
               </el-button>
             </template>
           </el-table-column>
@@ -61,8 +61,19 @@
         </el-table>
       </template>
 
+      <!-- 分页 -->
+      <el-pagination
+        background
+        layout="prev, pager, next"
+        :page-size="pageSize"
+        :current-page="isShowSubCategory ? currentPage1 : currentPage"
+        :total="isShowSubCategory ? subCategoryData.length : categoryData.length"
+        @current-change="currentChange($event)">
+      </el-pagination>
+      
+
       <!-- 添加品类 -->
-      <el-dialog title="添加分类" :visible.sync="dialogFormVisible">
+      <el-dialog title="添加分类" :visible.sync="showAddCategory">
         <el-form :model="form" ref="form" label-position="top" :rules="rules">
           <el-form-item label="所属分类">
             <el-select v-model="form.id" placeholder="请选择活动区域">
@@ -75,25 +86,48 @@
           </el-form-item>
         </el-form>
         <div slot="footer" class="dialog-footer">
-          <el-button @click="dialogFormVisible = false">取 消</el-button>
+          <el-button @click="showAddCategory = false">取 消</el-button>
           <el-button type="primary" @click="handleSubmit">确 定</el-button>
         </div>
       </el-dialog>
+
+      <!-- 修改分类名称 -->
+      <el-dialog title="更新分类" :visible.sync="showUpdateCategoryName" width="30%">
+        <el-form>
+          <el-form-item>
+            <el-input autocomplete="off" v-model="updateCategoryNameValue"></el-input>
+          </el-form-item>
+        </el-form>
+        <div slot="footer" class="dialog-footer">
+          <el-button @click="showUpdateCategoryName = false">取 消</el-button>
+          <el-button type="primary" @click="handleUpdateCategoryName">确 定</el-button>
+        </div>
+      </el-dialog>
+
     </el-card>
   </el-main>
 </template>
 
 <script>
-import { reqCategory } from '../../api'
+import { reqCategory, reqAddCategory, reqUpdateCategoryName } from '../../api'
 export default {
   data() {
     return {
       categoryData: [],
+      categoryPageData: [],      
       categoryName: '',
+      updateCategoryNameId: '',
+      updateCategoryNameValue: '',
+      updateCategoryNameType: '',
       subCategoryData: [],
       isShowSubCategory: false,
       // 添加品类的显示
-      dialogFormVisible: true,
+      showAddCategory: false,
+      showUpdateCategoryName: false,
+      pageSize: 4,
+      // 显示一级或二级当前分页，不能共用同一个当前页
+      currentPage: 1,
+      currentPage1: 1,
       form: {
         name: '',
         id: '0',
@@ -101,8 +135,7 @@ export default {
       rules: {
         name: [
           // 想要校验数字，必须在 v-model 上写 v-model.number，官方文档上已说明
-          { required: true, message: '必须填' },
-          { type: 'number', message: '填数字' }
+          { required: true, message: '必须填' }
         ]
       }
     }
@@ -110,10 +143,19 @@ export default {
   async mounted () {
     const result = await reqCategory({parentId: 0})
     this.categoryData = result.data
+    console.log(this.categoryData)
+    this.getPageData(1)
   },
   methods: {
-    updateCategoryName(a) {
-      console.log(a);
+    // 修改分类名称的显示 一级或二级 
+    // type 判断是一级还是二级 0为一级 1为二级
+    updateCategoryName(scope, type) {
+      // 展示修改名称组件
+      this.showUpdateCategoryName = true
+      const { _id, name } = scope.row
+      this.updateCategoryNameId = _id
+      this.updateCategoryNameValue = name
+      this.updateCategoryNameType = type
     },
     async showSubCategory(row) {
       const result = await reqCategory({parentId: row._id})
@@ -127,12 +169,62 @@ export default {
     },
     // 处理添加分类
     handleSubmit() {
-      this.$refs.form.validate((flag) => {
+      this.$refs.form.validate(async (flag) => {
         if (flag) {
-          console.log(this.form);
-          this.$refs.form.resetFields()
+          const { name, id } = this.form
+          const result = await reqAddCategory({categoryName: name, parentId: id})
+          if (result.status === 0) {
+            this.categoryData.push(result.data)
+            this.showAddCategory = false
+          } else {
+            console.log(result.msg);
+          }
         }
       })
+    },
+    // 处理修改分类名字
+    async handleUpdateCategoryName() {
+      const categoryId = this.updateCategoryNameId
+      const categoryName = this.updateCategoryNameValue
+      const params = { categoryId, categoryName }
+      const result = await reqUpdateCategoryName(params)
+      if (result.status === 0) {
+        // 判断是修改了一级分类还是二级分类
+        const categoryType = this.updateCategoryNameType === 0 ? 'categoryData' : 'subCategoryData'
+        // 修改分类数组数据
+        this[categoryType].find((item, index) => {
+          if (item._id === categoryId) {
+            item.name = categoryName
+          }
+        })
+        
+        this.$message({
+          message: '修改成功',
+          type: 'success'
+        });
+        this.showUpdateCategoryName = false
+      }
+    },
+    // 分页
+    getPageData(page) {
+      const pageSize = this.pageSize
+      const stringObj = JSON.stringify(this.categoryData)
+      const toObj = JSON.parse(stringObj)
+      this.categoryPageData = toObj.splice((page-1) * pageSize, pageSize)
+    },
+    // 页码变化时
+    currentChange(page) {
+      this.currentPage = page
+      this.getPageData(page)
+      // console.log(page);
+    }
+  },
+  watch: {
+    categoryData: {
+      handler() {
+        this.getPageData(this.currentPage)
+      },
+      deep: true
     }
   }
 }
